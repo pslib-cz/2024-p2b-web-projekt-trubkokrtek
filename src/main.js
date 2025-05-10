fead51708b68d908c0299f0324aee8e9dcfc90a5
trubkokrtek <agentdominik707@gmail.com>
2025-05-10 22:30:02 +0200

    added styles

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import 'normalize.css/normalize.css'
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/swiper-bundle.css';
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    loop: true,
+    modules: [Navigation, Pagination, Autoplay],
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    autoplay: {
+      delay: 5000,
+    },
+  });
