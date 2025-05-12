a8bf637d8e35d35625297f0971d3271a5c6324e6
Trubkokrtek <agentdominik707@gmail.com>
2025-05-12 12:26:00 +0200

    Updates

---
+++ assets/about-zjHJDnLa.js
@@ -0,0 +1,1 @@
+import"./style-DfI0l_JV.js";document.addEventListener("click",t=>{t.target.closest(".tile__button")&&(alert("Položka byla přidána do košíku"),t.preventDefault())});
---
+++ assets/contact-Dphpf7wm.js
@@ -0,0 +1,1 @@
+import"./style-DfI0l_JV.js";document.addEventListener("click",t=>{t.target.closest(".form__button")&&alert("Tvoje zpráva byla odeslána")});
