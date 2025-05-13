7c3cb4a7d50f820c1279548bf4fc8698cacd2a8a
trubkokrtek <agentdominik707@gmail.com>
2025-05-13 21:26:48 +0200

    Updates

--- assets/about-ChsUZBd7.js
+++
@@ -1,1 +0,0 @@
-import"./style-B9acUX4r.js";document.addEventListener("click",t=>{t.target.closest(".tile__button")&&(alert("Položka byla přidána do košíku"),t.preventDefault())});
--- assets/contact-D0PG-KSM.js
+++
@@ -1,1 +0,0 @@
-import"./style-B9acUX4r.js";document.addEventListener("click",t=>{t.target.closest(".form__button")&&alert("Tvoje zpráva byla odeslána")});
