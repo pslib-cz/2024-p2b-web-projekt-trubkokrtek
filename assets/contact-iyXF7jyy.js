1540bb3721c672945b0bc8cd293e448daa964834
trubkokrtek <agentdominik707@gmail.com>
2025-05-18 21:57:02 +0200

    Updates

---
+++ assets/contact-iyXF7jyy.js
@@ -0,0 +1,1 @@
+document.addEventListener("click",e=>{e.target.closest(".form__button")&&alert("Tvoje zpráva byla odeslána")});
---
+++ assets/shop-DsyPogT2.js
@@ -0,0 +1,1 @@
+document.addEventListener("click",t=>{t.target.closest(".tile__button")&&(alert("Položka byla přidána do košíku"),t.preventDefault())});
